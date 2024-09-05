5cf7efef44fc1bf057bb890c83aec24818d69a45
kewablay <kojokewajunior123@gmail.com>
2024-09-05 16:13:51 +0000

    added app component to test multiply by operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
+import { multiplyBy } from './operators/multiply-by.operator';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'custom-rxjs-operator-and-unit-testing';
+  values = [1, 2, 'three', 4, null]
+  finalValues: number[] = []
+
+  ngOnInit(){
+    of(...this.values).pipe(
+      multiplyBy(2)
+
+    ).subscribe((value) => {
+      if(typeof value === 'number'){
+        this.finalValues.push(value)
+      }
+    })
+  }
 }
